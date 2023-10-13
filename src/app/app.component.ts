80cfe65bc02e03d2a4cc65c29d03627419e854f4
DaradeNikita <Daradenikitahanumant13@>
2023-10-14 00:47:21 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'ioDecorator';
+  todoListArray : Array <string> = []
+  
+  addTodo(todo : HTMLInputElement){
+    let val = todo.value;
+    this.todoListArray.push(val);
+    todo.value = '';
+  }
+  getTodoItem(val :string){
+    this.todoListArray.push(val);
+  }
+
+  
 }
